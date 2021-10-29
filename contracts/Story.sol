7f466a689355a563b077fba580331f57942e9576
Nathan A <nathanan@Nathans-MBP.fios-router.home>
2021-10-28 21:17:52 -0400

    Change to Solidity version 0.8.0 and add getters to Story.sol

--- contracts/Story.sol
+++ contracts/Story.sol
@@ -1,11 +1,11 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.8.0;
  
-/**
+/*
  * @title Story Smart Contract
  * @notice Primary smart contract for running Storyline operations.
  */
 contract Story {
-  /**
+  /*
    * StoryElement Struct.
    * A single piece of Storyline content, published by a writer.
    * @dev Any metadata about story content should go here.
@@ -16,7 +16,7 @@ contract Story {
     string dateTime;
   }
 
-  /**
+  /*
    * StoryElement Master List.
    * List of StoryElement structs, in published order.
    * StoryLine should traverse this to build the latest story.
@@ -24,21 +24,21 @@ contract Story {
    */
   StoryElement[] story;
 
-  /**
+  /*
    * Next available StoryElement id.
    * This should realistically never reach the maximum representation.
    */
-  uint256 nextElementId = 1;
+  uint256 nextElementId;
 
-  /**
+  /*
    * Story contract constructor.
    * @dev This should initialize the default story to "Once upon a time "
    */
-  constructor() public {
-    // TODO: Implement me
+  constructor() {
+    nextElementId = 1;
   }
 
-  /**
+  /*
    * Initializes a new StoryElement struct and adds it to the story.
    * This should populate the fresh struct with the given values.
    * @param _content the text associated with the StoryElement
@@ -56,25 +56,31 @@ contract Story {
     nextElementId++;
 
     newElem.content = _content;
-
     newElem.dateTime = _dateTime;
 
     story.push(newElem);
   }
 
-  /**
-   * TODO: write this function header
+  /*
+   * getFullStory serves as a getter for the story
    */
-  /* TODO: Implement function
-  function getFullStory() public returns (StoryElement[]) {}
-  */
+  function getFullStory() public view returns (StoryElement[] memory) {
+    return story;
+  }
 
 
-  /**
-   * TODO: write this function header
+
+  /*
+   * getFullStory serves as a getter for an element within the story
+   * If story with _id is not found, create error of type Panic(uint256)
    */
-  /* TODO: Implement function
-  function getStoryElement(uint256 memory _id) public returns (StoryElement) {}
-  */
-
+  function getStoryElement(uint256 _id) public view returns (StoryElement memory) {
+    // @dev May need to restructure story for efficiency
+    for(uint256 i=0; i<story.length; i++) {
+        if (story[i].id == _id) {
+          return story[i];
+        }
+    }
+    revert('Not found');
+  }
 }
