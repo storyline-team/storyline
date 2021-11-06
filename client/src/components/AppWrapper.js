f9f95f48bf6329d47bda8c88515c907461db650c
Jonathan Cheng <jonathanchengjr77@gmail.com>
2021-11-06 17:39:24 -0400

    Able to now read and write to chain

--- client/src/components/AppWrapper.js
+++ client/src/components/AppWrapper.js
@@ -3,22 +3,23 @@ import '../App.css';
 
 // evergreen imports
 import { Pane, Heading, Paragraph } from 'evergreen-ui';
-
-const sampleText =
-  'On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.';
+import HeaderBar from './HeaderBar';
 
 const AppWrapper = (props) => {
   // unwrap props
   const { drizzle, drizzleState } = props;
 
-  const [story, setStory] = useState(null);
+  const [story, setStory] = useState([]);
   const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     let fetchStory = async () => {
       const storyContract = drizzle.contracts.Story;
-      let story = await storyContract.methods.getFullStory().call();
-      console.log(story);
+      let storyElems = await storyContract.methods.getFullStory().call();
+      let story = [];
+      storyElems.forEach((element) => {
+        story.push(element.content);
+      });
       setStory(story);
       setLoaded(true);
     };
@@ -28,29 +29,29 @@ const AppWrapper = (props) => {
   if (!loaded) {
     return (
       <Pane display='flexbox' flexDirection='column' padding='10%'>
-        <Heading size='900' paddingBottom='5%'>
+        <Heading size={900} paddingBottom='5%'>
           Fetching Story...
         </Heading>
       </Pane>
     );
   }
   return (
-    <Pane display='flexbox' flexDirection='column' padding='10%'>
-      <Heading size='900' paddingBottom='5%'>
-        Storyline
-      </Heading>
-      <div>
-        <Paragraph>{story[0].content}</Paragraph>
-      </div>
+    <Pane display='flexbox' flexDirection='row' padding='10%'>
+      <HeaderBar drizzle={drizzle} />
+      {story.map((text) => (
+        <div key={text}>
+          <Paragraph>{text}</Paragraph>
+        </div>
+      ))}
       {/* Example of splitting text based on full stop */}
-      {sampleText.split('. ').map((text) => (
+      {/* {sampleText.split('. ').map((text) => (
         <div>
           <Paragraph>
             {text}
             {text === '' ? null : '.'}
           </Paragraph>
         </div>
-      ))}
+      ))} */}
     </Pane>
   );
 };
